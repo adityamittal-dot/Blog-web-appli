3380a9eb2c51ec30751a092f060ef5f82d6a5532
adityamittal-dot <110041287+adityamittal-dot@users.noreply.github.com>
2025-03-20 10:17:41 +0530

    add post,put,delete,update

--- index.js
+++ index.js
@@ -25,15 +25,70 @@ app.get("/", (req, res) => {
     res.render("blog", { blogs });
 });
 
-// Single Blog Post Route
-app.get("/post/:id", (req, res) => {
-    const blog = blogs.find((b) => b.id == req.params.id);
-    if (blog) {
-        res.render("post", { blog });
-    } else {
-        res.send("Post Not Found");
-    }
-});
+const getPosts = () => {
+    const data = fs.readFileSync(postsFilePath, 'utf-8');
+    return JSON.parse(data);
+  };
+  
+  // Helper function to save posts
+  const savePosts = (posts) => {
+    fs.writeFileSync(postsFilePath, JSON.stringify(posts, null, 2));
+  };
+  
+  // Home Page - View all posts
+  app.get('/', (req, res) => {
+    const posts = getPosts();
+    res.render('home', { posts });
+  });
+  
+  // New Post Form
+  app.get('/posts/new', (req, res) => {
+    res.render('new');
+  });
+  
+  // Create Post
+  app.post('/posts', (req, res) => {
+    const posts = getPosts();
+    const newPost = { id: Date.now(), title: req.body.title, content: req.body.content };
+    posts.push(newPost);
+    savePosts(posts);
+    res.redirect('/');
+  });
+  
+  // Show Post
+  app.get('/posts/:id', (req, res) => {
+    const posts = getPosts();
+    const post = posts.find(p => p.id == req.params.id);
+    if (!post) return res.status(404).send('Post not found');
+    res.render('show', { post });
+  });
+  
+  // Edit Post Form
+  app.get('/posts/:id/edit', (req, res) => {
+    const posts = getPosts();
+    const post = posts.find(p => p.id == req.params.id);
+    if (!post) return res.status(404).send('Post not found');
+    res.render('edit', { post });
+  });
+  
+  // Update Post
+  app.put('/posts/:id', (req, res) => {
+    let posts = getPosts();
+    posts = posts.map(post =>
+      post.id == req.params.id ? { ...post, title: req.body.title, content: req.body.content } : post
+    );
+    savePosts(posts);
+    res.redirect('/');
+  });
+  
+  // Delete Post
+  app.delete('/posts/:id', (req, res) => {
+    let posts = getPosts();
+    posts = posts.filter(post => post.id != req.params.id);
+    savePosts(posts);
+    res.redirect('/');
+  });
+  
 
 // Start Server
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
